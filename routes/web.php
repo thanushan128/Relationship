7f50bf224112c58ec23a529d95cc8becefb0b0f8
w3schools-test <test@w3schools.com>
2023-08-14 08:47:48 +0530

    One to Many Relationship

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use Illuminate\Support\Facades\Response;
 use App\Models\Phone;
+use App\Models\Post;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,35 +17,42 @@ use App\Models\Phone;
 |
 */
 
+// one to one Relation
+// Route::get('/', function () {
+//     // $user = User::find(1);
+//     // dd($user->phone);
+
+//     // Get the output as json format
+//     // $user = User::with('phone')->whereId(1)->first();
+//     // return Response::json($user);
+
+//     // belongs to (inverse one to one relationship)
+//     // $phone = Phone::find(1);
+//     // dd($phone->user);
+
+//     // Get the output as json format
+//     // $phone = Phone::with('user')->whereId(1)->first();
+//     // return Response::json($phone);
+
+//     // Update records
+//         // $user = User::find(1);
+//         // $phone = new Phone;
+//         // $phone->phone = '0768888888';
+
+//     // $user->phone()->save($phone);
+//         // $user->phone()->update($phone->toArray());
+//         // return 'Saved!';
+
+//     // change the relationship record into another user record
+//         $phone = Phone::find(1);
+//         $user = User::find(2);
+
+//         $phone->user()->associate($user)->save();
+//         return 'Saved!';
+// });
+
+// one to many Realation
 Route::get('/', function () {
-    // $user = User::find(1);
-    // dd($user->phone);
-
-    // Get the output as json format
-    // $user = User::with('phone')->whereId(1)->first();
-    // return Response::json($user);
-
-    // belongs to (inverse one to one relationship)
-    // $phone = Phone::find(1);
-    // dd($phone->user);
-
-    // Get the output as json format
-    // $phone = Phone::with('user')->whereId(1)->first();
-    // return Response::json($phone);
-
-    // Update records
-        // $user = User::find(1);
-        // $phone = new Phone;
-        // $phone->phone = '0768888888';
-
-    // $user->phone()->save($phone);
-        // $user->phone()->update($phone->toArray());
-        // return 'Saved!';
-
-    // change the relationship record into another user record
-        $phone = Phone::find(1);
-        $user = User::find(2);
-
-        $phone->user()->associate($user)->save();
-        return 'Saved!';
+    $post = Post::find(1);
+    dd($post->comments);
 });
